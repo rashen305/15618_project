c9a2bc3aa0839b5bbe6c54c98edba24b03b71ed7
Albert Luo <albertlu@andrew.cmu.edu>
2026-04-12 20:20:33 -0400

    fix: Fix PE architecture to only use 1 cycle latency for now (should pipeline the MAC later); updated testbench

--- fpga/rtl/src/sa_processing_elem.sv
+++ fpga/rtl/src/sa_processing_elem.sv
@@ -16,8 +16,8 @@
 * A single systolic array PE.
 *
 * Parameters:
-*   - I_WORD_SIZE: Input word size. For acceptors, this is the matrix word
-*                  size, but for inner PEs, it will be double that.
+*   - I_WORD_SIZE: Input word size. Since this is for a NSSA, only the inputs
+*                  propagate.
 *
 * Inputs:
 *   - clk:         System clock -- we will try to run this as fast as possible.
@@ -42,7 +42,7 @@
 */
 module sa_processing_elem
     #(parameter int I_WORD_SIZE = MATRIX_WORD_SIZE,
-      parameter int O_WORD_SIZE = (I_WORD_SIZE * 2))
+      parameter int O_WORD_SIZE = I_WORD_SIZE)
     (input  logic                     clk,
      // TODO: Synchronous reset for now -- should probably change later.
      input  logic                     rst_l,
@@ -50,8 +50,8 @@ module sa_processing_elem
      input  logic                     i_acc_clear,
      input  logic [I_WORD_SIZE - 1:0] i_rowData,
      input  logic [I_WORD_SIZE - 1:0] i_colData,
-     output logic [O_WORD_SIZE - 1:0] o_rowData,
-     output logic [O_WORD_SIZE - 1:0] o_colData,
+     output logic [I_WORD_SIZE - 1:0] o_rowData,
+     output logic [I_WORD_SIZE - 1:0] o_colData,
      output logic [O_WORD_SIZE - 1:0] o_accData);
 
     logic [I_WORD_SIZE - 1:0] rowData, colData;
@@ -63,13 +63,13 @@ module sa_processing_elem
     register #(.WIDTH(I_WORD_SIZE))
         rowReg(.clk,
                .rst_l,
-               .clear(i_acc_clear),
+               .clear(1'b0),
                .en(i_valid),
                .regIn(i_rowData),
                .regOut(rowData)),
         colReg(.clk,
                .rst_l,
-               .clear(i_acc_clear),
+               .clear(1'b0),
                .en(i_valid),
                .regIn(i_colData),
                .regOut(colData));
@@ -87,8 +87,8 @@ module sa_processing_elem
 
     // Multiply inputs.
     multiplier #(.I_WIDTH(I_WORD_SIZE))
-        macMultiplier(.multIn1(rowData),
-                      .multIn2(colData),
+        macMultiplier(.multIn1(i_rowData),
+                      .multIn2(i_colData),
                       .multOut);
 
     // Accumulate with current C[i][j] value.
@@ -99,6 +99,6 @@ module sa_processing_elem
 
     assign o_rowData = rowData;
     assign o_colData = colData;
-    assign o_accData = accumulatorData;
+    assign o_accData = macOut;
 endmodule : sa_processing_elem
 `endif // _SA_PROCESSING_ELEM
--- fpga/rtl/test/single_pe_test.sv
+++ fpga/rtl/test/single_pe_test.sv
@@ -15,8 +15,11 @@ module single_pe_test();
     logic                 clk, rst_l;
     logic                 i_valid;
     logic                 i_acc_clear;
-    logic [I_WIDTH - 1:0] i_rowData, i_colData;
-    logic [O_WIDTH - 1:0] o_rowData, o_colData, o_accData;
+    logic [I_WIDTH - 1:0] i_rowData, i_colData, o_rowData, o_colData;
+    logic [O_WIDTH - 1:0] o_accData;
+
+    // Golden MAC output for verification.
+    logic [O_WIDTH - 1:0] golden_mac;
 
     sa_processing_elem #(
         .I_WORD_SIZE(I_WIDTH),
@@ -28,86 +31,33 @@ module single_pe_test();
     initial begin
         clk   = 0;
         rst_l = 1;
+        golden_mac = '0;
 
         forever #10 clk = ~clk;
     end
 
-    logic [O_WIDTH - 1:0] golden_mac;
     initial begin
         // Reset PE initially.
-        rst_l <= 1'b1;
-        i_valid <= 1'b0;
-        i_acc_clear <= 1'b0;
-        @(posedge clk);
-        rst_l <= 1'b0;
-        @(posedge clk);
-
-        RESET_ASSERT : begin
-            assert(processingElem_DUT.rowData == '0) else begin
-                $error("Row data should be cleared on reset.\n");
-            end
-            assert(processingElem_DUT.colData == '0) else begin
-                $error("Column data should be cleared on reset.\n");
-            end
+        @(posedge clk) begin
+            rst_l <= 1'b0;
+            i_valid <= 1'b0;
+            i_acc_clear <= 1'b1;
         end
 
-        rst_l <= 1'b1;
-        i_rowData <= I_WIDTH'(888);
-        i_colData <= I_WIDTH'(999);
-        @(posedge clk);
-        i_valid <= 1'b0;
-
-        NON_VALID_LATCH_ASSERT : begin
-            assert(processingElem_DUT.rowData != i_rowData) else begin
-                $error("Should not have latched data when i_valid = 0.\n");
-            end
-            assert(processingElem_DUT.colData != i_colData) else begin
-                $error("Should not have latched data when i_valid = 0.\n");
-            end
+        @(posedge clk) begin
+            rst_l <= 1'b1;
+            i_valid <= 1'b1;
+            i_acc_clear <= 1'b0;
         end
 
-        @(posedge clk);
-        i_valid <= 1'b1;
-
-        VALID_MUL_ASSERT : begin
-            assert(processingElem_DUT.rowData == i_rowData) else begin
-                $error("Should latch data on i_valid. Expected rowData = %d, but got %d.\n",
-                         i_rowData, processingElem_DUT.rowData);
-            end
-            assert(processingElem_DUT.colData == i_colData) else begin
-                $error("Should latch data on i_valid. Expected colData = %d, but got %d.\n",
-                         i_colData, processingElem_DUT.colData);
-            end
-            assert(o_accData == (888 * 999)) else begin
-                $error("Accumulator should be enabled on i_valid. Expected o_accData = %d, but got %d.\n",
-                         888 * 999, o_accData);
-            end
-        end
-
-        i_acc_clear <= 1'b0;
-        @(posedge clk);
-
-        CLEAR_ACC_ASSERT : begin
-            assert(o_accData == '0) else begin
-                $error("Did not clear accumulator on i_acc_clear.\n");
-            end
-        end
-
-        i_acc_clear <= 1'b0;
-        golden_mac <= '0;
-        i_rowData <= '0;
-        i_colData <= '0;
-        rst_l <= 1'b0;
-        @(posedge clk);
-        rst_l <= 1'b1;
-        @(posedge clk);
-
-        for (int i = 0; i < 3; i++) begin
+        for (int i = 0; i < 100; i++) begin
             i_rowData <= I_WIDTH'(i);
             i_colData <= I_WIDTH'(i + 1);
-            @(posedge clk);
             golden_mac <= (golden_mac + (i * (i + 1)));
 
+            @(posedge clk);
+
+            // Note that the accumulator data propagates one cycle later.
             RAND_COL_DATA_ASSERT : begin
                 assert(o_accData == golden_mac) else begin
                     $error("Expected o_colData = %d, got %d instead.\n",
@@ -115,20 +65,16 @@ module single_pe_test();
                 end
             end
 
-            PROP_ROW_COL_ASSERT : begin
-                assert(o_rowData == i_rowData) else begin
-                    $error("Should propagate input row data to output. Expected %d, but got %d.\n",
-                             i_rowData, o_rowData);
-                end
-                assert(o_colData == i_colData) else begin
-                    $error("Should propagate input column data to output. Expected %d, but got %d.\n",
-                             i_colData, o_colData);
-                end
-            end
         end
 
         // Extra cycles before finishing test -- for sanity.
         repeat (5) @(posedge clk);
+
+        $display("\n");
+        $display("***************************************************************************");
+        $display("                            ALL TESTS PASSED!                              ");
+        $display("***************************************************************************");
+        $display("\n");
 
         $finish;
     end
